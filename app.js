79a8047d263e67674cb78fab950e0f403e6853f1
azizdjan_official <tulaganovazizjon@gmail.com>
2023-10-29 04:35:14 +0500

    the homework is done

---
+++ app.js
@@ -0,0 +1,108 @@
+
+
+// let word = prompt("Iltimos so'zni kiriting");
+// let splitt = word.split("")
+// let reverse = splitt.reverse()
+// alert(reverse.join(""))
+
+// 1) Reverse string => "Lorem" -> "meroL" (For, prompt)
+// 2) Reverse number => 8658 -> 8568 (For, prompt) !NUMBER
+
+// *********************************************************************
+// *********************************************************************
+// *********************************************************************
+
+// let son = prompt("Iltimos sonni kiriting")
+// let realSon = son * 1;
+
+// if(realSon % 2 == 0){
+//     alert(realSon + ' juft')
+// }else{
+//     alert(realSon + ' toq')
+// }
+// 3) toq yoki juft => 10 -> 0 juft, 1 toq, ..., 9 toq, 10juft
+// *********************************************************************
+// *********************************************************************
+// *********************************************************************
+
+
+// let sonni = prompt('Sonni kiriting');
+// let sonn = sonni * 1 ;
+
+// if(sonn === 1){
+//     alert('murakkab son')
+// }else if(sonn === 2){
+//     alert('tub son')
+// }else
+// {
+//   for(let i = 2; i < sonn; i++)
+//   {
+//     if(sonn % i === 0)
+//     {
+//         alert('murakkabb son')
+//     }
+//   }
+//   alert('tubb son') 
+// }
+
+// 4) Tub sonmi => 3 -> tub, 4 -> murakkab, 9 -> murakkab
+// *********************************************************************
+// *********************************************************************
+// *********************************************************************
+
+
+
+
+// let num = prompt('Iltimos biror son kiriting qoshib beraman');
+// // let str = String(num)
+// let a = 0;
+
+// for( let i=0; i< num.length; i++)
+// {
+//     a =  a + Number(num[i]);
+// }
+// alert("Bu sonni qo'shganda " +a + ' soni chiqar ekan');
+
+// 6) masalaning javobi
+// *********************************************************************
+// *********************************************************************
+// *********************************************************************
+
+
+// let bormi = 'S';
+// let word = "Assalom alekum barchaga ";
+// let ajrataylik = word.split('')
+// bormi = bormi.toLowerCase();
+
+// let sana = 0;
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] === bormi) {
+//       sana++;
+//     }
+// }
+
+// if(ajrataylik.includes(bormi)){
+//     console.log("bu harf bor ekan va " + sana + ' marta ishlatilgan ekan');
+// }else{
+//     console.log('Bu harf yoq ekan va ' + sana + ' marta ishlatilgan ekan');
+// }
+
+// 7) masalaning javobi
+// *********************************************************************
+// *********************************************************************
+// *********************************************************************
+
+// let word = prompt("Iltimos so'zni kiriting")
+// let spl = word.split('')
+// let rev = spl.reverse().join('')
+
+
+// if(rev === word){
+//     alert(true);
+// }else{
+//     alert(false);
+// }
+// 8) masalaning javobi
+// *********************************************************************
+// *********************************************************************
+// *********************************************************************
